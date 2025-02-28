9bb351d250fb1cfeeb6e6471a1747e7d3ee5b8de
Himanshu Rai <himanshu2003rai@gmail.com>
2025-02-28 22:47:08 +0530

    Welcome hello

---
+++ src/main/java/net/engineeringdigest/Rest/controller/HelloMessenger.java
@@ -0,0 +1,19 @@
+package net.engineeringdigest.Rest.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/hello")
+public class HelloMessenger {
+
+    @GetMapping
+    public String welcomeAll(){
+        return "HELLO, from BridgeLabz";
+    }
+
+
+
+}
---
+++ src/main/java/net/engineeringdigest/Rest/entity/Message.java
@@ -0,0 +1,9 @@
+package net.engineeringdigest.Rest.entity;
+
+public class Message {
+
+    private String message;
+
+    public String getMessage(){return this.message;}
+    public void   setMessage(String _message){this.message=_message;}
+}
